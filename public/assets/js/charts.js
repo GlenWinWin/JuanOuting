37dd576ed30f5e9faf8bdeda631512f6a7ee13e1
GlenWinWin <glenwinbernabe@gmail.com>
2017-02-23 10:00:02 +0800

    Removed first line in charts.js

--- public/assets/js/charts.js
+++ public/assets/js/charts.js
@@ -1,4 +1,3 @@
-document.write('<script type="text/javascript" src="assets/js/charts/search1.js"><\/script>');
 document.write('<script src="assets/js/charts/pie/status.js"><\/script>');
 document.write('<script src="assets/js/charts/bar/sales.js"><\/script>');
 document.write('<script src="assets/js/charts/line/monthlySales.js"><\/script>');
