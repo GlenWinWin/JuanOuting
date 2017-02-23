5de82972f17acfbaf468488068ac6f1486ba880c
GlenWinWin <glenwinbernabe@gmail.com>
2017-02-23 09:55:16 +0800

    Edit the position of the graphs to center

--- resources/views/resortsOwners/dashboard.blade.php
+++ resources/views/resortsOwners/dashboard.blade.php
@@ -6,43 +6,45 @@
 
 @section('body-content')
 <div class="col-lg-9" style="margin-top:15px">
-  <div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;float:left;">
-        <canvas id="myPieChart" width="400" height="400"></canvas>
+  <center>
+    <div class="row">
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myPieChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myBarChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myLineChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myRadarChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myPolarChart" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myDoughnut" width="400" height="400"></canvas>
+        </div>
+      </div>
+      <div class="col-md-12 col-sm-12 col-xs-12">
+        <div class="x_panel" style="width:70%;border-color:white;">
+          <canvas id="myBubbleChart" width="400" height="400"></canvas>
+        </div>
       </div>
     </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myBarChart" width="400" height="400"></canvas>
-      </div>
-    </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myLineChart" width="400" height="400"></canvas>
-      </div>
-    </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myRadarChart" width="400" height="400"></canvas>
-      </div>
-    </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myPolarChart" width="400" height="400"></canvas>
-      </div>
-    </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myDoughnut" width="400" height="400"></canvas>
-      </div>
-    </div>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-      <div class="x_panel" style="width:70%;border-color:white;">
-        <canvas id="myBubbleChart" width="400" height="400"></canvas>
-      </div>
-    </div>
-  </div>
+  </center>
 </div>
 @stop
 @section('graphs')
